8f0016a18106551fc52b92fc0a377be5121baa02
Dario Casciato <dario.casciato@dormakaba.com>
2023-09-05 15:23:50 +0200

    added Logging functions, and mathutils

---
+++ utils/Logging/Logging.h
@@ -0,0 +1,16 @@
+// =============================================================================
+// Utils | Logging
+// =============================================================================
+
+#include <Arduino.h>
+#include <stdarg.h>
+
+#ifndef UTILS_LOGGING_
+#define UTILS_LOGGING_
+
+/// @brief Prints a message to the serial monitor
+///
+/// @param message The message that needs to be printed
+void log(const char *format, ...);
+
+#endif // UTILS_LOGGING_
