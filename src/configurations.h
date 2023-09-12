ae0cc8a11fa7506ae890269ebfc872f2b19403db
Dario Casciato <dario.casciato@dormakaba.com>
2023-09-12 11:27:09 +0200

    minor changes

--- src/configurations.h
+++ src/configurations.h
@@ -5,9 +5,14 @@
 #ifndef _TEMPLATE_PROJECT_CONFIGURATIONS_
 #define _TEMPLATE_PROJECT_CONFIGURATIONS_
 
+
+
+
+
+// wifi credentials (only for ESP8266)
 #ifdef ESP8266
 char ssid[32] = "";
 char password[32] = "";
-#endif  // ESP8266
+#endif // ESP8266
 
 #endif // _TEMPLATE_PROJECT_CONFIGURATIONS_
