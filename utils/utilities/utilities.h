d03eece84349a634aa990766f215f354c4c19662
Dario Casciato <dario.casciato@dormakaba.com>
2024-01-19 14:45:38 +0100

    Remove MathUtils.hrename utilities

--- utils/utilities/utilities.h
+++ utils/utilities/utilities.h
@@ -7,4 +7,11 @@
 
 #define ARR_ELEMENT_COUNT(a) (sizeof(a) / sizeof(*a))
 
+template <typename E>
+constexpr auto to_underlying(E e)
+{
+   static_assert(std::is_enum<E>::value);
+   return static_cast<typename std::underlying_type<E>::type>(e);
+}
+
 #endif
