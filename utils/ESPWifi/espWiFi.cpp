152ce87d624f78d9303548c52dccfbc6889d61b6
Dario Casciato <dario.casciato@dormakaba.com>
2024-01-30 08:20:18 +0100

    new wifi commands

--- utils/ESPWifi/espWiFi.cpp
+++ utils/ESPWifi/espWiFi.cpp
@@ -8,6 +8,37 @@
 #include "Logging.h"
 
 
+namespace
+{
+    const char* fixSsid = "SSID";
+    const char* fixPassword = "PASSWORD";
+} // namespace
+
+
+
+bool Wifi::establish()
+{
+    // Connect to the WiFi network
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(fixSsid, fixPassword);
+
+    for(uint8_t i = 0; i < 10; i++)
+    {
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            Logging::log("Connected to WiFi!");
+            return true;
+        }
+
+        delay(1000);
+        Logging::log("Connecting to WiFi...");
+    }
+
+    // If the connection failed, log an error
+    Logging::log("Failed to connect to WiFi!");
+    return false;
+}
+
 bool Wifi::establish(String ssid, String password)
 {
     // Connect to the WiFi network
@@ -52,6 +83,11 @@ bool Wifi::establish(char* ssid, char* password)
     // If the connection failed, log an error
     Logging::log("Failed to connect to WiFi!");
     return false;
+}
+
+void Wifi::disconnect()
+{
+    WiFi.disconnect();
 }
 
 bool Wifi::isConnected()
